2ade961d4b232793315b228c8189f33900835c92
Chad Metcalf <chad@wibidata.com>
2012-08-20 12:38:22 -0700

    Make purging the limits.d directory optional.
    Many packages install files into the limits.d directory. Allow the user
    to decide if they want to purge non puppet managed files.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,10 @@
-class limits::config {
+class limits::config ($manage_limitsd) {
     file { $limits::params::limits_dir:
         ensure  => directory,
         owner   => root,
         group   => root,
         force   => true,
-        purge   => true,
+        purge   => $manage_limitsd,
         recurse => true,
     }
 }
