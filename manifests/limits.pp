31293ea394238581668043d91bde538aa9dc641b
Steffen Zieger <me@saz.sh>
2012-10-04 01:34:18 +0200

    include limits::params class in limits::limits

--- manifests/limits.pp
+++ manifests/limits.pp
@@ -5,6 +5,8 @@ define limits::limits(
   $hard = undef,
   $soft = undef
 ) {
+  include limits::params
+
   file { "${limits::params::limits_dir}${name}":
     ensure  => $ensure,
     owner   => 'root',
