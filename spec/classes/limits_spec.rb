10e897cb10d37489f9bda8b02c39fc30f29f71e3
Steffen Zieger <me@saz.sh>
2022-07-14 10:46:05 +0200

    rubocop fixes

--- spec/classes/limits_spec.rb
+++ spec/classes/limits_spec.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 describe 'limits' do
   let :default_params do
     {
-      :manage_limits_d_dir => true,
-      :purge_limits_d_dir => true
+      manage_limits_d_dir: true,
+      purge_limits_d_dir: true
     }
   end
 
   [{},
    {
-     :purge_limits_d_dir => false
+     purge_limits_d_dir: false
    },
    {
-     :manage_limits_d_dir => false
+     manage_limits_d_dir: false
    }].each do |param_set|
     describe "when #{param_set == {} ? 'using default' : "specifying #{param_set}"} class parameters" do
       let :param_hash do
@@ -26,7 +28,7 @@ describe 'limits' do
       %w[Debian Gentoo RedHat Suse].each do |osfamily|
         let :facts do
           {
-            :osfamily => osfamily
+            osfamily: osfamily
           }
         end
 
@@ -42,7 +44,7 @@ describe 'limits' do
                 'force'   => true,
                 'recurse' => true,
                 'purge'   => param_hash[:purge_limits_d_dir]
-)
+              )
             end
           end
         end
--- spec/defines/limits_spec.rb
+++ spec/defines/limits_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
-describe 'limits::limits', :type => :define do
+describe 'limits::limits', type: :define do
   let(:filename) { '/etc/security/limits.d/username_nofile.conf' }
 
   let :facts do
     {
-      :osfamily => 'Debian'
+      osfamily: 'Debian'
     }
   end
 
@@ -12,29 +14,31 @@ describe 'limits::limits', :type => :define do
     let(:title) { 'username_nofile' }
     let :params do
       {
-        :user => 'username',
-        :limit_type => 'nofile',
-        :ensure     => 'present',
-        :hard       => 16_384,
-        :soft       => 16_384
+        user: 'username',
+        limit_type: 'nofile',
+        ensure: 'present',
+        hard: 16_384,
+        soft: 16_384
       }
     end
 
     describe 'when creating a limits entry not ending in .conf' do
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_class('limits') }
+
       it {
-        is_expected.to contain_limits__limits('username_nofile').with(:user => 'username',
-                                                                      :limit_type => 'nofile',
-                                                                      :ensure     => 'present',
-                                                                      :hard       => 16_384,
-                                                                      :soft       => 16_384)
+        is_expected.to contain_limits__limits('username_nofile').with(user: 'username',
+                                                                      limit_type: 'nofile',
+                                                                      ensure: 'present',
+                                                                      hard: 16_384,
+                                                                      soft: 16_384)
       }
+
       it {
         is_expected.to contain_file(filename).with('ensure' => 'present',
-                                                   'content'   => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nusername      hard  nofile          16384\nusername      soft  nofile          16384\n",
-                                                   'owner'     => 'root',
-                                                   'group'     => 'root')
+                                                   'content' => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nusername      hard  nofile          16384\nusername      soft  nofile          16384\n",
+                                                   'owner' => 'root',
+                                                   'group' => 'root')
       }
     end
 
@@ -42,94 +46,98 @@ describe 'limits::limits', :type => :define do
       let(:title) { 'username_nofile.conf' }
 
       it {
-        is_expected.to contain_limits__limits('username_nofile.conf').with(:user => 'username',
-                                                                           :limit_type => 'nofile',
-                                                                           :ensure     => 'present',
-                                                                           :hard       => 16_384,
-                                                                           :soft       => 16_384)
+        is_expected.to contain_limits__limits('username_nofile.conf').with(user: 'username',
+                                                                           limit_type: 'nofile',
+                                                                           ensure: 'present',
+                                                                           hard: 16_384,
+                                                                           soft: 16_384)
       }
+
       it {
         is_expected.to contain_file(filename).with('ensure' => 'present',
-                                                   'content'   => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nusername      hard  nofile          16384\nusername      soft  nofile          16384\n",
-                                                   'owner'     => 'root',
-                                                   'group'     => 'root')
+                                                   'content' => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nusername      hard  nofile          16384\nusername      soft  nofile          16384\n",
+                                                   'owner' => 'root',
+                                                   'group' => 'root')
       }
     end
 
     describe 'when removing an limits entry' do
       let :params do
         {
-          :user       => 'username',
-          :limit_type => 'nofile',
-          :ensure     => 'absent',
-          :hard       => 16_384,
-          :soft       => 16_384
+          user: 'username',
+          limit_type: 'nofile',
+          ensure: 'absent',
+          hard: 16_384,
+          soft: 16_384
         }
       end
 
       it {
         is_expected.to contain_file(filename).with('ensure' => 'absent',
-                                                   'owner'     => 'root',
-                                                   'group'     => 'root')
+                                                   'owner' => 'root',
+                                                   'group' => 'root')
       }
     end
 
     describe 'when removing an limits entry without hard or soft set' do
       let :params do
         {
-          :user       => 'username',
-          :limit_type => 'nofile',
-          :ensure     => 'absent'
+          user: 'username',
+          limit_type: 'nofile',
+          ensure: 'absent'
         }
       end
 
       it {
         is_expected.to contain_file(filename).with('ensure' => 'absent',
-                                                   'owner'     => 'root',
-                                                   'group'     => 'root')
+                                                   'owner' => 'root',
+                                                   'group' => 'root')
       }
     end
 
     context 'when creating a limits entry setting both limits' do
       let :params do
         {
-          :user       => 'username',
-          :limit_type => 'nofile',
-          :ensure     => 'present',
-          :both       => 16_384
+          user: 'username',
+          limit_type: 'nofile',
+          ensure: 'present',
+          both: 16_384
         }
       end
       let(:title) { 'username_nofile.conf' }
 
       it {
-        is_expected.to contain_limits__limits('username_nofile.conf').with(:user => 'username',
-                                                                           :limit_type => 'nofile',
-                                                                           :ensure     => 'present',
-                                                                           :both       => 16_384)
+        is_expected.to contain_limits__limits('username_nofile.conf').with(user: 'username',
+                                                                           limit_type: 'nofile',
+                                                                           ensure: 'present',
+                                                                           both: 16_384)
       }
+
       it {
         is_expected.to contain_file(filename).with('ensure' => 'present',
-                                                   'content'   => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nusername      -     nofile          16384\n",
-                                                   'owner'     => 'root',
-                                                   'group'     => 'root')
+                                                   'content' => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nusername      -     nofile          16384\n",
+                                                   'owner' => 'root',
+                                                   'group' => 'root')
       }
     end
   end
+
   context 'title pattern' do
     describe 'default/nofile' do
       let(:title) { '*/nofile' }
       let :params do
         {
-          :both       => 16_384
+          both: 16_384
         }
       end
 
       it { is_expected.to compile.with_all_deps }
+
       it {
         is_expected.to contain_file('/etc/security/limits.d/default_nofile.conf').with('ensure' => 'present',
-                                                                                       'content'   => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\n*             -     nofile          16384\n",
-                                                                                       'owner'     => 'root',
-                                                                                       'group'     => 'root')
+                                                                                       'content' => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\n*             -     nofile          16384\n",
+                                                                                       'owner' => 'root',
+                                                                                       'group' => 'root')
       }
     end
 
@@ -137,15 +145,15 @@ describe 'limits::limits', :type => :define do
       let(:title) { 'root/nofile' }
       let :params do
         {
-          :hard       => 12_345
+          hard: 12_345
         }
       end
 
       it {
         is_expected.to contain_file('/etc/security/limits.d/root_nofile.conf').with('ensure' => 'present',
-                                                                                    'content'   => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nroot          hard  nofile          12345\n",
-                                                                                    'owner'     => 'root',
-                                                                                    'group'     => 'root')
+                                                                                    'content' => "# Managed by Puppet\n\n#<domain>    <type> <item>          <value>\nroot          hard  nofile          12345\n",
+                                                                                    'owner' => 'root',
+                                                                                    'group' => 'root')
       }
     end
   end
@@ -155,24 +163,26 @@ describe 'limits::limits', :type => :define do
       let(:title) { 'nofile' }
       let :params do
         {
-          :limit_type => 'nofile',
-          :both       => 16_384
+          limit_type: 'nofile',
+          both: 16_384
         }
       end
 
       it { is_expected.to compile.and_raise_error(%r{when not using the title pattern}) }
     end
+
     describe 'missing type' do
       let(:title) { 'nofile.conf' }
       let :params do
         {
-          :user => 'foo',
-          :both => 16_384
+          user: 'foo',
+          both: 16_384
         }
       end
 
       it { is_expected.to compile.and_raise_error(%r{when not using the title pattern}) }
     end
+
     describe 'missing limit' do
       let(:title) { 'root/nofile' }
 
